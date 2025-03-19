8c5db7e569c2d42d70fb0d60af35676f9c3c76a2
Elena Ionela Schipor <elena.schipor2001@gmail.com>
2025-03-19 17:48:19 +0100

    perfezionamenti es2 + completamento file quadrature

--- esercitazioni/ex02/ex_02_mio.jl
+++ esercitazioni/ex02/ex_02_mio.jl
@@ -4,17 +4,17 @@ using LinearAlgebra
 using SparseArrays
 using LaTeXStrings
 using Plots
-using Printf
 using Revise
 includet("C:/Users/elena_/Documents/GitHub/elementifiniti2425/modules/Meshing.jl")
 
 # on meshes\
 begin
         
-    mesh_square(0.3)
-    mesh_circle(0.3)
+    mesh_square(0.3, display = true)
+    mesh_circle(0.3, display = true)
+end
 
-
+begin
     # cambiare eventualmente a square_003 o circle 003 per avere mesh più piccole (ci ho messo 5 minuti a fare i conti non rifacciamoli)
     T_circle_ind, T_circle_points= get_nodes_connectivity("tmp_circle.msh")
 
@@ -27,72 +27,41 @@ begin
     function plot_mesh(T_ind, T_points, b_ind, b_p)
         p = plot(axis = false, ratio = 1)
         N_tri = size(T_ind, 2)
-        N_points = size(T_points, 2)
         N_bounds = size(b_ind,1)
-        # list_points_x = []
-        # list_points_y = []
-        boundary_nodes = []
-        for j in 1:N_bounds
-            push!(boundary_nodes, b_p[:, j])
-        end
+        boundary_nodes = [b_p[:, j] for j in 1:N_bounds]
         for i in 1:N_tri
             i1, i2, i3 = T_ind[:, i]
-            
             x1, y1 = T_points[:, i1]
             x2, y2 = T_points[:, i2]
             x3, y3 = T_points[:, i3]
-            # append!(list_points_x, [x1, x2, x3])
-            # append!(list_points_y, [y1,y2,y3])
-            plot!([x1,x2], [y1, y2], label="", lw=2, color=:green)
-            plot!([x2, x3], [y2,y3], label="", lw=2, color=:green)
-            plot!([x3,x1], [y3,y1], label="", lw=2, color=:green)
-
+            plot!([x1,x2,x3,x1], [y1, y2, y3, y1], label="", lw=2, color=:green)
             if [x1, y1] in boundary_nodes && [x2, y2] in boundary_nodes
                 plot!([x1,x2], [y1, y2], label="", lw=2, color=:blue)
             end
             if [x2, y2] in boundary_nodes && [x3, y3] in boundary_nodes
                 plot!([x2,x3], [y2, y3], label="", lw=2, color=:blue)
             end
-
             if [x3, y3] in boundary_nodes && [x1, y1] in boundary_nodes
                 plot!([x3,x1], [y3,y1], label="", lw=2, color=:blue)
-            end
-
-
-            
+            end 
         end
-
         display(p)
-        # plot(list_points_x, list_points_y, label = "", color=:green)
-        # return Nothing
     end
-
-
     plot_mesh(T_circle_ind, T_circle_points, b_circle_ind, b_circle_p)
     plot_mesh(T_square_ind, T_square_points, b_square_ind, b_square_p)
-
 end
+
+T_003, P_003 =get_nodes_connectivity("circle_003.msh") 
+b_ind_003, b_p_003 = get_boundary_nodes("circle_003.msh")
+plot_mesh(T_003, P_003, b_ind_003, b_p_003)
 
 import Meshes
 mesh = to_Meshes(T_circle_ind, T_circle_points)
 Meshes.viz(mesh, showsegments = true)
 
+##### QUADRATURE
 
-
- begin
-    function u1(x)
-        return exp(x[1]+x[2])
-    end
-    function u2(x)
-        return x[1]^2 + x[2]^2
-    end
-    function u3(x)
-        return exp(x[1]^2+x[2]^2)
-    end
-    function u4(x)
-        return 1
-    end
-
+begin
     function triarea(V1, V2, V3)
         area = 0.5 * abs(V1[1] * (V2[2] - V3[2]) + V2[1] * (V3[2] - V1[2]) + V3[1] * (V1[2] - V2[2]))
         return area
@@ -104,12 +73,11 @@ Meshes.viz(mesh, showsegments = true)
         for i in 1:N_tri
             i1, i2, i3 = T_ind[:, i]
             
-            x1, y1 = T_points[:, i1]
-            x2, y2 = T_points[:, i2]
-            x3, y3 = T_points[:, i3]
+            v1 = T_points[:, i1]
+            v2 = T_points[:, i2]
+            v3= T_points[:, i3]
             
-            ba[i,1] = (x1+x2+x3)/3
-            ba[i, 2] =  (y1+y2+y3)/3
+            ba[i,:] = (v1+v2+v3)./3
         end
         return ba
     end
@@ -120,7 +88,6 @@ Meshes.viz(mesh, showsegments = true)
         Q = 0
         for i in 1:N_tri
             i1, i2, i3 = T_ind[:, i]
-            
             V1 = T_points[:, i1]
             V2 = T_points[:, i2]
             V3 = T_points[:, i3]
@@ -135,7 +102,6 @@ Meshes.viz(mesh, showsegments = true)
         Q = 0
         for i in 1:N_tri
             i1, i2, i3 = T_ind[:, i]
-            
             V1 = T_points[:, i1]
             V2 = T_points[:, i2]
             V3 = T_points[:, i3]
@@ -151,7 +117,6 @@ Meshes.viz(mesh, showsegments = true)
         Q = 0
         for i in 1:N_tri
             i1, i2, i3 = T_ind[:, i]
-            
             V1 = T_points[:, i1]
             V2 = T_points[:, i2]
             V3 = T_points[:, i3]
@@ -159,7 +124,6 @@ Meshes.viz(mesh, showsegments = true)
             m2 = (V3+V2)/2
             m3 = (V1+V3)/2
             area_T = triarea(V1, V2, V3) 
-
             Q += area_T * ( u(m1) + u(m2) + u(m3))/3
         end
         return Q
@@ -175,45 +139,51 @@ Meshes.viz(mesh, showsegments = true)
         end
     end
 end
-# test
 
 
-i_es_sq = [exp(2)-2*exp(1)+1, 2/3] 
-i_es_cr = [pi *(exp(1)-1), pi] 
+u1 = (x) -> exp(x[1]+x[2])
+u2 = (x) ->  x[1]^2 + x[2]^2
+u3 = (x) -> exp(x[1]^2+x[2]^2)
+u4 = (x) -> 1
 
-u_sq = [u1, u2]
-u_cr = [u3, u4]
+# test with specific case
+i_es= [exp(2)-2*exp(1)+1, 2/3, pi *(exp(1)-1), pi] 
 
-errors = zeros(15, 3, 4)
-H = LinRange(10^-2, 1, 15)
-for j in 1:15 
+u_n= [u1, u2, u3, u4]
+
+f = (x) -> 2*x
+
+errors = zeros(10, 3, 4)
+# H = LinRange(10^-2, 1, 15)
+H = 10 .^ range(-2, 0, length=10)
+for j in 1:10 
     h = H[j]
     mesh_square(h)
     mesh_circle(h)
     T_ind_S, T_points_S = get_nodes_connectivity("tmp_square.msh")
     T_ind_C, T_points_C = get_nodes_connectivity("tmp_circle.msh")
     
-    for z in 1:2
-        u = u_sq[z]
-        integrale_esatto = i_es_sq[z]
-        for i in 0:2
-            q = quadratura(u, T_ind_S, T_points_S, i)
-            errors[j, i+1, z] = abs(q-integrale_esatto)
+    for l in 1:4
+        if l in 1:2
+            z = l
+            u = u_n[z]
+            integrale_esatto = i_es[l]
+            for i in 0:2
+                q = quadratura(u, T_ind_S, T_points_S, i)
+                errors[j, i+1, l] = abs(q-integrale_esatto)
+            end
+        else
+            z = l-2
+            u = u_n[l]
+            integrale_esatto = i_es[l]
+            for i in 0:2
+                q = quadratura(u, T_ind_C, T_points_C, i)
+                errors[j, i+1, l] = abs(q-integrale_esatto)
+            end
         end
     end
-    for z in 1:2
-        u = u_cr[z]
-        integrale_esatto = i_es_cr[z]
-        for i in 0:2
-            q = quadratura(u, T_ind_C, T_points_C, i)
-            errors[j, i+1, z+2] = abs(q-integrale_esatto)
-        end
-    end
-    # errors = hcat(errors, err)
     println(h, "-------------------------")
 end
-
-println(errors)
 
 etichette = [L"Errore con $u(x,y)= e^{x+y}$ sul quadrato", L"Errore con $u(x,y)=x^2+y^2$ sul quadrato", 
 L"Errore con $u(x,y)=e^{x^2+y^2}$ sul cerchio", L"Errore con $u(x,y) = 1$ sul cerchio"]
@@ -234,17 +204,4 @@ for z in 1:4
     display(p)
 end
 
-for z in 1:2
-    p = plot()
-    for i in 0:2
-        x = H
-        y = errors[:, i+1, z+2]
-        inds = y.>0
-        plot!(x[inds], y[inds], xscale = :log10, yscale=:log10, label = "errore, i="*string(i), lw = 2, ms = 2)
-        title!(L"Errore di funzione $z = %$(z+2)$  su quadrato")
-    end
-    plot!(H, H.^2,  xscale = :log10, yscale=:log10, label = "rif 2 ord", lw = 2, ms = 2, ls = :dash)
-    # plot!(H, exp(-5).*H.^3 , xscale = :log10, yscale=:log10, label = "rif 3 ord", lw = 2, ms = 2, ls = :dash)
-    display(p)
-end
 
--- modules/Quadrature.jl
+++ modules/Quadrature.jl
@@ -19,7 +19,29 @@ function triarea(V1, V2, V3)
 end
 
 """
-    Q0(p, T, u)
+    b(T, p):
+
+    returns array of baricenter points of the triangles
+"""
+function b(T_ind, T_points)
+    N_tri = size(T_ind, 2)
+    ba = zeros(N_tri, 2)
+    for i in 1:N_tri
+        i1, i2, i3 = T_ind[:, i]
+        
+        v1 = T_points[:, i1]
+        v2 = T_points[:, i2]
+        v3= T_points[:, i3]
+        
+        ba[i,:] = (v1+v2+v3)./3
+    end
+    return ba
+end
+
+
+
+"""
+    Q0(u, T, p)
 
 Perform numerical integration using the Q0 quadrature rule (i.e., baricenter formula) over a mesh.
 This quadrature rule has order 1.
@@ -32,12 +54,23 @@ This quadrature rule has order 1.
 # Returns
 - `I_approx::Float64`: The approximate integral of the function over the mesh.
 """
-function Q0(p, T, u)
-    
+function Q0(u, T_ind, T_points)
+    baricentro =  b(T_ind, T_points)
+    N_tri = size(T_ind, 2)
+    Q = 0
+    for i in 1:N_tri
+        i1, i2, i3 = T_ind[:, i]
+        V1 = T_points[:, i1]
+        V2 = T_points[:, i2]
+        V3 = T_points[:, i3]
+        area_T = triarea(V1, V2, V3) 
+        Q += area_T * u(baricentro[i, :])
+    end
+    return Q
 end
 
 """
-    Q1(p, T, u)
+    Q1(u, T, p)
 
 Perform numerical integration using the Q1 quadrature rule (i.e., vertex formula) over a mesh.
 This quadrature rule has order 1.
@@ -50,9 +83,21 @@ This quadrature rule has order 1.
 # Returns
 - `I_approx::Float64`: The approximate integral of the function over the mesh.
 """
-function Q1(p, T, u)
-    
+function Q1(u, T_ind, T_points)
+    N_tri = size(T_ind, 2)
+    Q = 0
+    for i in 1:N_tri
+        i1, i2, i3 = T_ind[:, i]
+        V1 = T_points[:, i1]
+        V2 = T_points[:, i2]
+        V3 = T_points[:, i3]
+        area_T = triarea(V1, V2, V3) 
+
+        Q += area_T * ( u(V1) + u(V2) + u(V3))/3
+    end
+    return Q
 end
+
 
 """
     Q2(p, T, u)
@@ -68,6 +113,35 @@ This quadrature rule has order 2.
 # Returns
 - `I_approx::Float64`: The approximate integral of the function over the mesh.
 """
-function Q2(p, T, u)
-    
+
+function Q2(u, T_ind, T_points)
+    N_tri = size(T_ind, 2)
+    Q = 0
+    for i in 1:N_tri
+        i1, i2, i3 = T_ind[:, i]
+        V1 = T_points[:, i1]
+        V2 = T_points[:, i2]
+        V3 = T_points[:, i3]
+        m1 = (V1+V2)/2
+        m2 = (V3+V2)/2
+        m3 = (V1+V3)/2
+        area_T = triarea(V1, V2, V3) 
+        Q += area_T * ( u(m1) + u(m2) + u(m3))/3
+    end
+    return Q
+end
+
+"""
+quadratura(u, T, P, i)
+performs either Q0 or Q1 or Q2 depending on the index i given
+"""
+
+function quadratura(u, T_ind, T_points, i)
+    if i == 0
+        return Q0(u, T_ind, T_points)
+    elseif i == 1
+        return Q1(u, T_ind, T_points)
+    elseif i == 2
+        return Q2(u, T_ind, T_points)
+    end
 end
