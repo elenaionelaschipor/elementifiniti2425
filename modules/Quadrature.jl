d31ca47c0e89924c8417f7511913b92882388893
Ivan Bioli <91244037+IvanBioli@users.noreply.github.com>
2025-03-28 07:38:21 +0100

    Solutions ex02

--- modules/Quadrature.jl
+++ modules/Quadrature.jl
@@ -33,7 +33,19 @@ This quadrature rule has order 1.
 - `I_approx::Float64`: The approximate integral of the function over the mesh.
 """
 function Q0(p, T, u)
-    
+    n_tri = size(T, 2) # Number of triangles
+    I_approx::Float64 = 0
+    for i = 1:n_tri
+        # Get the vertices
+        V1 = p[:, T[1, i]]
+        V2 = p[:, T[2, i]]
+        V3 = p[:, T[3, i]]
+        # Barycenter
+        G = (V1 + V2 + V3) ./ 3
+        # Contribution of the triangle of vertices V1, V2, V3
+        I_approx += u(G) * triarea(V1, V2, V3)
+    end
+    return I_approx
 end
 
 """
@@ -51,7 +63,17 @@ This quadrature rule has order 1.
 - `I_approx::Float64`: The approximate integral of the function over the mesh.
 """
 function Q1(p, T, u)
-    
+    n_tri = size(T, 2) # Number of triangles
+    I_approx::Float64 = 0
+    for i = 1:n_tri
+        # Get the vertices
+        V1 = p[:, T[1, i]]
+        V2 = p[:, T[2, i]]
+        V3 = p[:, T[3, i]]
+        # Contribution of the triangle of vertices V1, V2, V3
+        I_approx += (u(V1) + u(V2) + u(V3)) / 3 * triarea(V1, V2, V3)
+    end
+    return I_approx
 end
 
 """
@@ -69,5 +91,19 @@ This quadrature rule has order 2.
 - `I_approx::Float64`: The approximate integral of the function over the mesh.
 """
 function Q2(p, T, u)
-    
+    n_tri = size(T, 2) # Number of triangles
+    I_approx::Float64 = 0
+    for i = 1:n_tri
+        # Get the vertices
+        V1 = p[:, T[1, i]]
+        V2 = p[:, T[2, i]]
+        V3 = p[:, T[3, i]]
+        # Mid-Points
+        M1 = (V1 + V2) ./ 2
+        M2 = (V1 + V3) ./ 2
+        M3 = (V2 + V3) ./ 2
+        # Contribution of the triangle of vertices V1, V2, V3
+        I_approx += (u(M1) + u(M2) + u(M3)) / 3 * triarea(V1, V2, V3)
+    end
+    return I_approx
 end
