838dc0621ef699ed6e6f0bdd51a6c08749ae9071
Elena Ionela Schipor <elena.schipor2001@gmail.com>
2025-04-25 10:27:59 +0200

    Update Quadrature_adv.jl

--- modules/Quadrature_adv.jl
+++ modules/Quadrature_adv.jl
@@ -80,8 +80,17 @@ function Quadrature(u, mesh::Mesh, ref_quad::TriQuad)
         # println(k, size(w_cap), size(u(p)))
 
         Q_k[k] = dot(w_cap, u(p))*detBk[k]
+        if k < 40
+            # println("Mia quadratura: primi valori")
+            # println(Q_k[k])
+            println("MIO")
+            println(u(p))
+            
+        end
+    
     end
     return sum(Q_k)
+end
 
 function Quadrature_soluzione(u, mesh::Mesh, ref_quad::TriQuad)
     # Compute matrices for pushforward of reference element
@@ -92,17 +101,20 @@ function Quadrature_soluzione(u, mesh::Mesh, ref_quad::TriQuad)
     points_refelem, weights_refelem = ref_quad.points, ref_quad.weights
     points_elem = zeros(Float64, size(points_refelem))
     u_evals = zeros(Float64, size(weights_refelem))
-
+    
     # Loop across all elements
     n_tri = size(mesh.T, 2)
     I_approx::Float64 = 0
     for i = 1:n_tri
         points_elem = Bk[:, :, i] * points_refelem .+ ak[:, i] # Points in the current element
         u_evals = eval_u(u, points_elem, mesh, i, ref_quad)
+        if i < 40
+            println("SOLUZIONE")
+            println(u_evals)
+        end
         I_approx += sum(u_evals .* weights_refelem) * detBk[i]
     end
     return I_approx
-end
 end
 
 # Evaluation of a function
