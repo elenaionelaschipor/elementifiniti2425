df861cf95f079ab4fdcf6f8844c78fb63449e57c
Elena Ionela Schipor <elena.schipor2001@gmail.com>
2025-05-10 16:17:29 +0200

    Update ex_05.jl

--- esercitazioni/ex05/ex_05.jl
+++ esercitazioni/ex05/ex_05.jl
@@ -190,3 +190,13 @@ end
 
 
 
+begin
+    scatter(H, err_dirichlet_inf, label = "errore al variare di h " , yscale=:log10, xscale =:log10)
+    plot!(H, H.^0.5 , xscale =:log10, yscale=:log10, label = "rif o(sqrt(h))")
+    xaxis!("h")
+    yaxis!("Errore")
+    title!("Errore in norma L2")
+end
+
+
+
