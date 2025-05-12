da3985a3cea7ed9a6d959cc413b33e09f709de91
Elena Ionela Schipor <elena.schipor2001@gmail.com>
2025-05-12 16:07:14 +0200

    Update ex_05.jl

--- esercitazioni/ex05/ex_05.jl
+++ esercitazioni/ex05/ex_05.jl
@@ -152,7 +152,7 @@ begin
         # vuol dire che f è fissato, tutto il resto no quindi va in input delle altre cose
         A, b = assemble_global(msh, local_assembler)
 
-        A_cond, b_cond, u_h = impose_dirichlet(A, b, g, msh)
+        _, _, u_h = impose_dirichlet(A, b, g, msh)
         
         quadratura = Q2_ref
         err_l2 = L2error(u_ex,u_h,msh,quadratura)
